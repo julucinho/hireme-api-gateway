307dab0aa203c5737105426253b0f34c0536a8e1
Julucin <joselucioalmeidajunior@gmail.com>
2022-01-10 21:23:47 -0300

    feat: starting project

---
+++ src/test/java/com/capitolio/hiremeapigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capitolio.hiremeapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
